9b2d6eb439d8aa7775d2d9eea2433737a95ac750
pdc <pengdongchu@kaytion.com>
2020-06-18 11:21:36 +0800

    Match Iphone X

--- 业务功能/start.常见前端错误提示/index.js
+++ 业务功能/start.常见前端错误提示/index.js
@@ -1,3 +1,7 @@
 // 1. JS错误 Uncaught SyntaxError: Unexpected token u in JSON at position 0
 // 在解析的时候出现 undefined 时，会出现此类报错 
 let result = JSON.parse(undefined);
+
+// 2.iphone x 适配底部问题
+// 文档：https://www.cnblogs.com/WQLong/p/7778368.html
+// https://juejin.im/post/5d6cb884f265da03ae788f28
