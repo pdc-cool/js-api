cffedc31b133f3cd64c6cdfeb9bc2e035aae950a
pdc <pengdongchu@kaytion.com>
2020-06-19 17:32:43 +0800

    Vue test

--- JS知识点/vue.js
+++
@@ -1,4 +0,0 @@
-// v-model 原理：bind 属性和 value 语法糖
-// <input v-model="inputValue"></input> 
-// <input @input="inputValue = $event.target.value" :value="inputValue"></input>
-
---
+++ vue知识点操作/01.组件同信/index.js
@@ -0,0 +1,2 @@
+let Bus = new Vue();
+
