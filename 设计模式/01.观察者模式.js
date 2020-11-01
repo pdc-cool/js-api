db04da08f57342de397d8f5c223852fbcef2106f
pdc <18158896930@163.com>
2020-11-01 11:39:13 +0800

    'fix:修改观察者模式知识点'

--- 设计模式/01.观察者模式.js
+++ 设计模式/01.观察者模式.js
@@ -1,7 +1,7 @@
 /*
  * @Author: pdc
  * @Date: 2020-11-01 11:12:35
- * @LastEditTime: 2020-11-01 11:24:11
+ * @LastEditTime: 2020-11-01 11:32:05
  * @LastEditors: Please set LastEditors
  * @Description: 观察者模式简介
  * @FilePath: \js-api\设计模式\01.观察者模式.js
@@ -11,6 +11,7 @@
 简介：观察者模式,当对象间存在一对多关系时，则使用观察者
 模式（Observer Pattern）。比如，当一个对象被修改时，则
 会自动通知依赖它的对象。观察者模式属于行为型模式。
-例如 token 处理中，token 就是这个对象，当 token 更新时，
-执行观察者 subscribers 数组中的回调函数
+例如 token 处理中，token 即观察目标，当 token 改变时，
+执行 subscribers(内部的回调函数即为观察者) 数组中的回调函数，
+观察目标和观察者是一对多的关系
 ----------------------------------------------------*/
