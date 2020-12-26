1b7b5e546adc710bfa440586c4a274d8aec602bc
pdc <18158896930@163.com>
2020-12-26 11:43:00 +0800

    '增加'

--- 常用算法/snippet.js
+++ 常用算法/snippet.js
@@ -3,9 +3,22 @@
  * @Descripttion: 优秀简介代码片段
  * @Date: 2020-12-26 11:01:55
  * @LastEditors: pengdongchu
- * @LastEditTime: 2020-12-26 11:03:59
+ * @LastEditTime: 2020-12-26 11:18:13
  */
 
 /* 统计字符串出现字母的次数 */
-const str = 'sfhjasfjgfasjuwqrqadqeiqsajsdaiwqdaklldflas-cmxzmnha';
-const res = str.split('').reduce((accumulator, cur) => {accumulator[cur] ? accumulator[cur]++ : accumulator[cur] = 1; return accumulator;}, {});
+const str = `sfhjasfjgfasjuwqrqadqeiqsajsdaiwqdaklldflas-cmxzmnha`;
+const res = str.split('').reduce((accumulator, cur) => {
+  accumulator[cur] ? accumulator[cur]++ : (accumulator[cur] = 1);
+  return accumulator;
+}, {});
+
+/* 数组 --> 对象 */
+const streams = [
+  { name: '技术', id: 1 },
+  { name: '设计', id: 2 },
+];
+const obj = streams.reduce((accumulator, cur) => {
+  accumulator[cur.id] = cur;
+  return accumulator;
+}, {});
